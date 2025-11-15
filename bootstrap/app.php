dadd5a1e02517a271656c2aadf0c27035466ec15
ilyas-yildiz <ilyasyildiz444@gmail.com>
2025-11-15 10:59:06 +0300

    fix: app.php update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,21 +3,19 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\AdminMiddleware; // YENİ EKLENDİ
+use App\Http\Middleware\AdminMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        home: '/admin/dashboard'
+        health: '/up'
+        // 'home: ...' SATIRI BURADAN KALDIRILDI. HATA BURADAYDI.
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // YENİ EKLENDİ: 'admin' kısayolunu AdminMiddleware'e bağlıyoruz
         $middleware->alias([
             'admin' => AdminMiddleware::class,
             'bayi'  => \App\Http\Middleware\BayiMiddleware::class,
-            // 'adminer' => \Onecentlin\Adminer\Http\Middleware\Authenticate::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
@@ -25,4 +23,6 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withProviders([
         \App\Providers\ViewServiceProvider::class,
-    ])->create();
+    ])
+    ->redirectUsersTo('/admin/dashboard')
+    ->create();
