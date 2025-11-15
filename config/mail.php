ab6a33871a73eab3b703c821f02db940935db4e3
ilyas-yildiz <ilyasyildiz444@gmail.com>
2025-11-15 10:10:36 +0300

    fix: mail settings update

--- config/mail.php
+++ config/mail.php
@@ -38,16 +38,17 @@ return [
     'mailers' => [
 
         'smtp' => [
-            'transport' => 'smtp',
-            'scheme' => env('MAIL_SCHEME'),
-            'url' => env('MAIL_URL'),
-            'host' => env('MAIL_HOST', '127.0.0.1'),
-            'port' => env('MAIL_PORT', 2525),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
-            'timeout' => null,
-            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
-        ],
+    'transport' => 'smtp',
+    'scheme' => env('MAIL_SCHEME'),
+    'url' => env('MAIL_URL'),
+    'host' => env('MAIL_HOST', '127.0.0.1'),
+    'port' => env('MAIL_PORT', 2525),
+    'encryption' => env('MAIL_ENCRYPTION', 'tls'), 
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
+    'timeout' => null,
+    'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+],
 
         'ses' => [
             'transport' => 'ses',
@@ -111,7 +112,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', null),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
